e7a6366f7d8bc87ab302a86cd56f76e11bc6770e
webdeveloper104433 <web.developer104433@gmail.com>
2021-08-08 04:21:35 +0300

    feat: remove login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule
