5c42c4ad636f29462add46717a455ca5caf63a7d
webdeveloper104433 <web.developer104433@gmail.com>
2021-08-08 04:25:49 +0300

    Revert "Merge pull request #1 from webdeveloper104433/build_without_welcome"
    This reverts commit d6790329efa34d6483dd22c60095f688ae68f3ab, reversing
    changes made to bd72ee26991e15579658361010668aa58061e71e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
