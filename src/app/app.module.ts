563f06122d13ee93d6d0d0897673229aed7e6721
webdeveloper104433 <web.developer104433@gmail.com>
2021-08-08 10:37:03 +0300

    feat: add remember component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RememberComponent } from './login/remember/remember.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RememberComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/login/remember/remember.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remember',
+  templateUrl: './remember.component.html',
+  styleUrls: ['./remember.component.css']
+})
+export class RememberComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
